1e1a5d5971a81a0b705de11042e26268c7c5386f
hemadry01 <sumanb843@gmai.com>
2026-09-11 13:02:55 +0600

    create dev stack project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import { FaHome } from 'react-icons/fa';
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <FaHome />
+      <button className="btn btn-success">Success</button>
+      <h2 className="text-2xl font-bold text-red-500">Dev Stack Project</h2>
+    </>
+  );
+}
+
+export default App
