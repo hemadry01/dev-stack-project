0742aeb20d1487535cbbfd3e1029b851d8d4538e
hemadry01 <sumanb843@gmai.com>
2026-09-11 16:00:08 +0600

    add nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 
 import { FaHome } from 'react-icons/fa';
 import './App.css'
+import { Nav } from './component/Nav';
 
 function App() {
   
 
   return (
     <>
-      <FaHome />
-      <button className="btn btn-success">Success</button>
-      <h2 className="text-2xl font-bold text-red-500">Dev Stack Project</h2>
+      <Nav/>
     </>
   );
 }
